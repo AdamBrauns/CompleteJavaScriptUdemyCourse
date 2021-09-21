1bbec66ed01d0bdd1095739acefdaf2f22db169b
Adam Brauns <braunsaj14@uww.edu>
2021-09-21 18:35:23 -0500

    finished string literals

--- 01_javascript_fundamentals_part_1/script2.js
+++ 01_javascript_fundamentals_part_1/script2.js
@@ -11,3 +11,40 @@ console.log(x, y);
 
 const avgAge = ( ageJonas + ageSara ) / 2;
 console.log(ageJonas, ageSara, avgAge);
+
+// Strings and Template Literals
+const firstName = 'Jonas';
+const job = 'Teacher';
+const birthYear = 1991;
+
+const jonas = "I'm " + firstName + ', a ' + ( currentYear - birthYear ) + ' year old ' + job + '!';
+console.log(jonas); 
+
+// String literal
+const jonasNew = `I'm ${firstName}, a ${currentYear - birthYear} year old ${job}!`;
+console.log(jonasNew);
+
+// You can also use it without variables (some devs are going this route)
+console.log(`Normal string`);
+
+// For multi-lines, string literal is much cleaner
+console.log('String with \n\
+multiple\n\
+lines');
+
+console.log(`String with
+multiple
+lines`);
+
+// Assignment 5 Strings and Template Literals
+const country = 'Portugal';
+const population = 11;
+const language = 'portuguese';
+// Old way:
+//const description = country + ' is in Europe, and its ' + population + ' million people speak ' + language;
+//console.log(description);
+
+// New way:
+const description = `${country} is in Europe, and its ${population} million people speak ${language}`;
+console.log(description);
+// End Assignment 5 Strings and Template Literals
