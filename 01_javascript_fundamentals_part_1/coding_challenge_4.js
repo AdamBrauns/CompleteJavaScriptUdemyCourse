de1f14916634350e82d5386a169939aed5e44cc9
Adam Brauns <braunsaj14@uww.edu>
2021-09-25 18:20:57 -0500

    finished coding challenge 4

---
+++ 01_javascript_fundamentals_part_1/coding_challenge_4.js
@@ -0,0 +1,10 @@
+// Data 1
+const billAmt = 275;
+
+/* Other test data
+const billAmt = 40;
+const billAmt = 430;
+*/
+
+const tipAmt = billAmt >= 50 && billAmt <= 300 ? billAmt * 0.15 : billAmt * 0.20;
+console.log(`The bill was $${billAmt}, the tip was $${tipAmt}, and the total value was $${billAmt + tipAmt}`);
