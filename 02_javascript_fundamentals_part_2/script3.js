ad28f8ceef5cc3106f2a5e1f89f924328e30c0d8
Adam Brauns <braunsaj14@uww.edu>
2021-09-26 22:26:44 -0500

    added script3

---
+++ 02_javascript_fundamentals_part_2/script3.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// Object Methods
+const joe = {
+  firstName: 'Joe',
+  lastName: 'Schmoe',
+  birthYear: 1983,
+  job: 'teacher',
+  friends: ['Frank', 'Larry', 'Joe'],
+  hasDriversLicense: false,
+
+  calcAge: function () {
+    this.age = 2037 - this.birthYear;
+    return this.age;
+  },
+  
+  getSummary: function () {
+    return `${this.firstName} is a ${this.calcAge()}-year old ${this.job}, and he has ${this.hasDriversLicense ? 'a' : 'no'} driver's license.`;
+  }
+};
+
+console.log(joe.calcAge());
+// console.log(joe['calcAge']()); 
+console.log(joe.age);
+console.log(joe.age);
+
+console.log(joe.getSummary());
