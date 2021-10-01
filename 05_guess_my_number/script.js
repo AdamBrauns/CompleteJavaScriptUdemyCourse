4490ded2be30ad3a6f1eee24fcd41612ff7f14fe
Adam Brauns <braunsaj14@uww.edu>
2021-09-30 20:55:36 -0500

    logic for decreasing number of guesses

--- 05_guess_my_number/script.js
+++ 05_guess_my_number/script.js
@@ -1,7 +1,22 @@
 'use strict';
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+// Temporarily show the secret number
 document.querySelector('.number').textContent = secretNumber;
+
+const decreaseScore = function (guess, highLow) {
+  if (score > 1) {
+    document.querySelector('.message').textContent = `Too ${highLow}...`;
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else {
+    document.querySelector('.message').textContent =
+      'You lost... Better luck next time...';
+    document.querySelector('.score').textContent = 0;
+  }
+};
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -12,8 +27,8 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number!';
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = 'Too High...';
+    decreaseScore(guess, 'High');
   } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = 'Too Low...';
+    decreaseScore(guess, 'Low');
   }
 });
