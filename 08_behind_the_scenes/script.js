bbb7c07e20847eb50961d9cb8c77e42b690a7844
Adam Brauns <braunsaj14@uww.edu>
2021-10-12 21:24:53 -0500

    finished hoisting sections

--- 08_behind_the_scenes/script.js
+++ 08_behind_the_scenes/script.js
@@ -39,3 +39,52 @@ calcAge(1991);
 // Will not work as its out of scope
 // console.log(age);
 // printAge();
+
+// Hoisting example
+
+// Variables
+console.log(me);
+// This is the temporal dead zone
+// console.log(job);
+// console.log(year);
+
+var me = 'Jonas';
+let job = 'teacher';
+const year = 1991;
+
+// Functions
+console.log(addDecl(2, 3));
+// Temporal dead zone
+// console.log(addExp(2, 3));
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+// Changing to var will be hoisted and set to undefined
+const addExp = function (a, b) {
+  return a + b;
+};
+// Changing to var will be hoisted and set to undefined
+const addArrow = (a, b) => a + b;
+
+// Example of why hoisting can lead to issues
+
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted!');
+}
+
+// Example for let const var
+var x = 1;
+let y = 2;
+const z = 3;
+
+// var is the only one that puts it into the window object
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
