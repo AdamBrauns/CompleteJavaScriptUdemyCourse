068045d0eb65141c08cc5409f6cbccfb1ce5fc0b
Adam Brauns <braunsaj14@uww.edu>
2021-10-21 22:08:39 -0500

    finished primitive vs reference

--- 08_behind_the_scenes/script2.js
+++ 08_behind_the_scenes/script2.js
@@ -45,7 +45,26 @@ addExpr(2, 5, 6, 12);
 
 // Arguments does not exist in arrow functions
 var addArrow = (a, b) => {
-  console.log(arguments);
+  //console.log(arguments);
   return a + b;
 };
 addArrow(2, 5, 7);
+
+// Primitives vs reference types
+let age = 30;
+let oldAge = age;
+age = 31;
+
+console.log(age);
+console.log(oldAge);
+
+const me = {
+  name: 'Jonas',
+  age: 30,
+};
+
+const friend = me;
+friend.age = 28;
+
+console.log('Me: ', me);
+console.log('Friend: ', friend);
