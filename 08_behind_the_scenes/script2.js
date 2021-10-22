039feec9cd92940e3dbb59e26ecddc2547e290ce
Adam Brauns <braunsaj14@uww.edu>
2021-10-21 21:46:35 -0500

    finished functions vs arrow functions

---
+++ 08_behind_the_scenes/script2.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var firstName = 'Matilda';
+
+const jonas = {
+  firstName: 'Jonas',
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+
+    // Solution 1
+    // const self = this; // Sometimes "that" is also used
+    // const isMillennial = function () {
+    //   console.log(self);
+    //   console.log(self.year >= 1981 && self.year <= 1996);
+    // };
+
+    // Solution 2
+    // Works because arrow function inherits the "this" from parent
+    const isMillennial = () => {
+      console.log(this);
+      console.log(this.year >= 1981 && this.year <= 1996);
+    };
+    isMillennial();
+  },
+  greet: () => {
+    console.log(this);
+    console.log(`Hey ${this.firstName}`);
+  },
+};
+
+jonas.greet(); // Prints "Hey Matilda"
+console.log(this.firstName); // Prints Matilda
+jonas.calcAge();
+
+// Arguments
+const addExpr = function (a, b) {
+  console.log(arguments);
+  return a + b;
+};
+
+addExpr(2, 5);
+addExpr(2, 5, 6, 12);
+
+// Arguments does not exist in arrow functions
+var addArrow = (a, b) => {
+  console.log(arguments);
+  return a + b;
+};
+addArrow(2, 5, 7);
