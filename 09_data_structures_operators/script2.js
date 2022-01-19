8d6536703aa7e569d60d2133df19ce5555e39199
Adam Brauns <braunsaj14@uww.edu>
2022-01-18 21:34:45 -0600

    finished nullish section

--- 09_data_structures_operators/script2.js
+++ 09_data_structures_operators/script2.js
@@ -143,3 +143,11 @@ if (restaurant.orderPizza) {
 }
 
 restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
+
+restaurant.numGuests1 = 0;
+const guests = restaurant.numGuests1 || 10;
+console.log(guests);
+
+// Nullish: null and undefined (NOT 0 or '')
+const guestCorrect = restaurant.numGuests1 ?? 10;
+console.log(guestCorrect);
