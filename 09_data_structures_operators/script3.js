bf311abd2fbb17643ab8916f5ee3c0ee9672eb02
Adam Brauns <braunsaj14@uww.edu>
2022-01-25 21:33:58 -0600

    finished map fundamentals section

--- 09_data_structures_operators/script3.js
+++ 09_data_structures_operators/script3.js
@@ -125,3 +125,34 @@ console.log(staffUnique);
 
 console.log(new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size);
 console.log(new Set('jonasschmedtmann').size);
+
+// Map
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+
+// The set method returns the map
+console.log(rest.set(2, 'Lisbon, Portugal'));
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :D')
+  .set(false, 'We are closed :(');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+// Power of having boolean as map keys
+const time = 21;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+const arr = [1, 2];
+rest.set(arr, 'Test');
+rest.set(document.querySelector('h1'), 'Heading');
+console.log(rest);
+console.log(rest.size);
+console.log(rest.get(arr));
